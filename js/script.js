32b7342b9cd3da1ee1fea449f19879656d99b121
Brandon <battleb2014@gmail.com>
2021-01-12 20:36:48 -0500

    search setup

--- js/script.js
+++ js/script.js
@@ -1,78 +1,151 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
+function showPage(list, page) {
 
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-
-{/* <li class="student-item cf">
-          <div class="student-details">
-            <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
-            <h3>Ethel Dean</h3>
-            <span class="email">ethel.dean@example.com</span>
-          </div>
-          <div class="joined-details">
-            <span class="date">Joined 12-15-2005</span>
-          </div>
-        </li> */}
-
-
-
-function showPage() {
    const ul = document.querySelector('ul');
-   const li = document.createElement('li');
-   li.className = 'student-item cf';
+   ul.innerHTML = ''; //Start with a blank page
 
-   const studentDiv = document.createElement('div');
-   studentDiv.className = 'student-details';
+   const firstIndex = (page * 9) - 10; //Index of first li to display
+   const lastIndex = page * 9; //Index of last li to display
 
-   const img = document.createElement('img');
-   img.className = 'avatar';
-   img.src = 'https://randomuser.me/api/portraits/women/25.jpg';
-   img.alt = 'Profile Picture';
+   for (let i = 0; i < list.length; i++) { //Loop through data object
+      if (i > firstIndex && i < lastIndex) { //Display only 9 li per page
+         const li = document.createElement('li'); //Creates li tag
+         li.className = 'student-item cf'; //Sets li tags class name
+
+         const studentDiv = document.createElement('div'); //Creates div tag
+         studentDiv.className = 'student-details'; //Sets div tags class name
+
+         const img = document.createElement('img'); //Creates img tag
+         img.className = 'avatar'; //Sets img tag name to avatar
+         img.src = list[i].picture.thumbnail; //Sets img src to appropriate link
+         img.alt = 'Profile Picture'; //Sets img alt 
+
+         const h3 = document.createElement('h3'); //Creates a h3 tag
+
+         const emailSpan = document.createElement('span'); //Creates a span tag
+         emailSpan.className = 'email'; //Sets spans class name
+         emailSpan.textContent = list[i].email; //Sets spans text content to appropriate email address
+
+         const detailsDiv = document.createElement('div'); //Creates a div tag
+         detailsDiv.className = 'joined-details'; //Sets divs class name
+
+         const dateSpan = document.createElement('span'); //Creates a span tag
+         dateSpan.className = 'date'; //Sets spans class name
+         dateSpan.textContent = list[i].registered.date; ////Sets divs text content
+
+         //Adds appropriate tags to page
+         ul.appendChild(li);
+         li.appendChild(studentDiv);
+         li.appendChild(detailsDiv);
+         studentDiv.appendChild(img);
+         studentDiv.appendChild(h3);
+         studentDiv.appendChild(emailSpan);
+         detailsDiv.appendChild(dateSpan);
+      }
+   }
+}
+
+function addPagination(list) {
+
+   const ul = document.querySelector('.link-list'); //REfrences appropriate ul tag
+   const numOfButtons = Math.ceil(list.length / 9); //Number of buttons displayed
+
+   for (let i = 0; i < numOfButtons; i++) { //Displays appropriate number of buttons
+
+      const li = document.createElement('li'); //Creates li tag
+      ul.appendChild(li); //Adds li to page
+
+      const button = document.createElement('button'); //Creates button tag
+      button.type = 'button'; //Sets button type to button
+      button.textContent = i + 1; //Sets buttons text content to appropriate number
+      li.appendChild(button); //Adds button to page
+   }
+
+   const firstButton = ul.querySelector('button'); //Identifies first button
+   firstButton.className = 'active'; //Sets first button as active by default
+
+   ul.addEventListener('click', (e) => { // Assign active class onclick and show corrisponding page
+      const active = ul.querySelector('.active'); //Selects button with the class name of active
+      active.className = ''; //Sets currently active buttons class name to an empty string
+      e.target.className = 'active'; //Sets clicked buttons class name to active
+
+      showPage(e.target.textContent); //Displays appropriate content
+   });
+}
+
+{/* <label for="search" class="student-search">
+            <input id="search" placeholder="Search by name...">
+            <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+          </label> */}
+
+function notFound() {
+   const studentUl = document.querySelector('.student-list');
+   const paginateUl = document.querySelector('.link-list');
+   const paginateLi = paginateUl.querySelector('li');
 
    const h3 = document.createElement('h3');
+   h3.textContent = 'Student not found';
 
-   const emailSpan = document.createElement('span');
-   emailSpan.className = 'email';
-   emailSpan.textContent = 'ethel.dean@example.com';
+   studentUl.appendChild(h3);
 
-   const detailsDiv = document.createElement('div');
-   detailsDiv.className = 'joined-details';
-
-   const dateSpan = document.createElement('span');
-   dateSpan.className = 'date';
-   dateSpan.textContent = 'Joined 12-15-2005';
-
-   ul.appendChild(li);
-   li.appendChild(studentDiv);
-   li.appendChild(detailsDiv);
-   studentDiv.appendChild(img);
-   studentDiv.appendChild(h3);
-   studentDiv.appendChild(emailSpan);
-   detailsDiv.appendChild(dateSpan);
+   for (let i = 0; i < paginateLi.length; i++) { //Loop through and remove all existing paginated links
+      paginateUl.removeChild(paginateLi[i]);
+   }
 }
-console.log(showPage());
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
 
-function addPagination() {
+function addSearchBar(list) {
+   const header = document.querySelector('.header'); //Selects the header tag
 
+   const label = document.createElement('label'); //Creates label tag
+   label.for = 'search';
+   label.className = 'search';
+
+   const searchInput = document.createElement('input'); //Creates input tag
+   searchInput.id = 'search';
+   searchInput.placeholder = 'Search by name...';
+
+   const searchButton = document.createElement('button'); //Creates button tag
+   searchButton.type = 'button';
+
+   const img = document.createElement('img'); //Creates img tag
+   img.src = 'img/icn-search.svg'; //Sets imgs src
+   img.alt = 'Search Icon'; //Sets imgs alt
+
+   //Adds search bar to page
+   header.appendChild(label);
+   label.appendChild(searchInput);
+   label.appendChild(searchButton);
+   searchButton.appendChild(img);
+
+
+   function search() {
+      const inputVal = searchInput.value.toLowerCase();
+      const search = [];
+
+      for (let i = 0; i < list.length; i++) {
+         let name = list[i].name.first.toLowerCase();
+         name += ' ';
+         name += list[i].name.last.toLowerCase();
+
+         if (name.includes(inputVal)) {
+            search.push(list[i]);
+         }
+
+         if (search.length > 0) {
+            showPage(search, 1);
+            addPagination(search);
+         } else {
+            notFound();
+         }
+
+      }
+   }
+
+   searchInput.addEventListener('keyup', search);
+   searchButton.addEventListener('click', search);
 }
 
 // Call functions
+
+showPage(data, 1);
+addPagination(data);
+addSearchBar(data);
